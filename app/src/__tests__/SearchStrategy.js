292a7e998ea7c4570770ced46f142793b92e27be
oliwuaa <118117671+oliwuaa@users.noreply.github.com>
2024-04-18 19:34:30 +0200

    Update SearchStrategy.js

--- app/src/__tests__/SearchStrategy.js
+++ app/src/__tests__/SearchStrategy.js
@@ -1,6 +1,22 @@
 const DrinkBuilder = require('../src/DrinkBuilder.js');
 const Drink = require('../src/Drink.js');
 
+
+const Builder = new DrinkBuilder(); 
+const mockDrinks = [
+  Builder.setName('Brainteaster').setIngredients(['Sambuca', 'Advocaat', 'Erin Cream']).build(),
+  Builder.setName('Snowball').setIngredients(['Lemonade', 'Advocaat', 'Lemon']).build(),
+  Builder.setName('Kiss me Quick').setIngredients(['Cranberry Vodka', 'Apfelkorn', 'Apple Juice', 'Schweppes Russchian']).build(),
+  Builder.setName('Angel Face').setIngredients(['Apricot brandy', 'Apple brandy', 'Gin']).build(),
+  Builder.setName('Jack Rose Cocktail').setIngredients(['Grenadine', 'Apple brandy', 'Lime']).build(),
+  Builder.setName('White Wine Sangria').setIngredients(['White Wine', 'Apple brandy', 'Soda Water', 'Fruits']).build(),
+  Builder.setName('Zimadori Zinger').setIngredients(['Zima', 'Midori melon liqueur']).build(),
+  Builder.setName('Zima Blaster').setIngredients(['Chambord raspberry liqueur', 'Zima']).build(),
+  Builder.setName('Mojito').setIngredients(['Rum', 'Lime', 'Mint', 'Sugar']).build(),
+  Builder.setName('Aperol Spritz').setIngredients(['Aperol', 'Prosecco', 'Soda Water', 'Orange Slice']).build(),
+  Builder.setName('Old Cuban').setIngredients(['Rum', 'Mint', 'Lime', 'Simple Syrup', 'Angostura Bitters', 'Champagne']).build(),
+];
+
 test('should return list of drinks which contain selected ingredients ', () => {
     const SearchByIngredients = new SearchByIngredients();
     const ingredientsToSearch = ['Advocaat', 'Sambuca', 'Erin Cream'];
