e2b9286c45a3005696f9ca71d631616a0093623f
oliwuaa <118117671+oliwuaa@users.noreply.github.com>
2024-04-18 16:08:29 +0200

    Create SearchStrategy.js

---
+++ app/src/SearchStrategy.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SearchStrategy extends React.Component {
+    // Placeholder interface for search strategy
+    constructor() {}
+  
+    search(searchQuery, dataSource) {
+      // To be implemented by specific search strategies
+      return [];
+    }
+  }
+
+export default SearchStrategy;
