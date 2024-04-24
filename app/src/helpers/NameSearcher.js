220cbf49f79d7e9aafd0b6c5b02835a708c15fda
Pola Walczynska <pola.walczynska@gmail.com>
2024-04-24 12:17:23 +0200

    fix searching by name

--- app/src/helpers/NameSearcher.js
+++ app/src/helpers/NameSearcher.js
@@ -17,7 +17,15 @@ const useNameSearcher = () => {
                 //fetiching results from the api
                 const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`);
                 const data = await response.json();
-                setSearchResults(data.drinks || []);
+
+                //filter the results
+                const filteredResults = data.drinks ? data.drinks.filter(drink => {
+                    const lowerCaseName = drink.strDrink.toLowerCase();
+                    const lowerCaseSearchTerm = searchTerm.toLowerCase();
+                    return lowerCaseName.startsWith(lowerCaseSearchTerm) || lowerCaseName.includes(` ${lowerCaseSearchTerm}`);
+                }) : [];
+
+                setSearchResults(filteredResults);
             } catch (error) {
                 console.error('Error fetching drinks:', error);
             }
