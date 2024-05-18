e0e95c8b38adee0d2d112892aeced08959084a68
Pola Walczynska <pola.walczynska@gmail.com>
2024-05-18 17:12:00 +0200

    fix styling

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import BeerIcon from '../assets/beer.png';
-import HamburgerIcon from '../assets/hamburger.png';
+import hamburger from '../assets/hamburger.png';
 import { Link, useLocation } from 'react-router-dom';
 import '../styles/Navbar.css';
 
@@ -16,24 +16,27 @@ function Navbar() {
     };
 
     return (
-        <div className='navbar'>
-            <div className='left'>
-                <Link to='/'>
-                    <img src={BeerIcon} alt="Home" />
-                </Link>
-            </div>
-            <div className='center'>
-                <div className={`menuLinks ${isMenuOpen ? 'active' : ''}`}>
-                    <Link to='/search-by-name' onClick={toggleMenu} className={IsActiveLink('/search-by-name')}>Search by name</Link>
-                    <Link to='/search-by-ingredients' onClick={toggleMenu} className={IsActiveLink('/search-by-ingredients')}>Search by ingredients</Link>
-                    <Link to='/all-recipes' className={useLocation().pathname === '/all-recipes' ? 'active' : ''}>All Recipes</Link>
+        <div>
+            <div className={`navbar ${isMenuOpen ? 'menuOpen' : ''}`}>
+                <div className='left'>
+                    <Link to='/'>
+                        <img src={BeerIcon} alt="Home" />
+                    </Link>
+                </div>
+                <div className='center'>
+                    <div className={`menuLinks ${isMenuOpen ? 'active' : ''}`}>
+                        <Link to='/search-by-name' onClick={toggleMenu} className={IsActiveLink('/search-by-name')}>Search by name</Link>
+                        <Link to='/search-by-ingredients' onClick={toggleMenu} className={IsActiveLink('/search-by-ingredients')}>Search by ingredients</Link>
+                        <Link to='/all-recipes' className={useLocation().pathname === '/all-recipes' ? 'active' : ''}>All Recipes</Link>
+                    </div>
+                </div>
+                <div className='right'>
+                    <div className={`menuIcon ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                        <img src={hamburger} alt="Menu" />
+                    </div>
                 </div>
             </div>
-            <div className='right'>
-                <div className={`menuIcon ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
-                    <img src={HamburgerIcon} alt="Menu" />
-                </div>
-            </div>
+            <div className={`navbarOverlay ${isMenuOpen ? 'active' : ''}`}></div>
         </div>
     );
 }
