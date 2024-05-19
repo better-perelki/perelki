9bb0ada3da840850e446d0611b295966f395a964
Pola Walczynska <pola.walczynska@gmail.com>
2024-05-19 13:27:01 +0200

    scrollUp visibility

--- app/src/components/ScrollUp.js
+++ app/src/components/ScrollUp.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaAngleUp } from 'react-icons/fa';
 import '../styles/ScrollUp.css';
 
 const ScrollToTop = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        if (window.pageYOffset > 500) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
@@ -10,8 +20,15 @@ const ScrollToTop = () => {
         });
     };
 
+    useEffect(() => {
+        window.addEventListener('scroll', toggleVisibility);
+        return () => {
+            window.removeEventListener('scroll', toggleVisibility);
+        };
+    }, []);
+
     return (
-        <div className="top-to-btm" onClick={scrollToTop}>
+        <div className={`top-to-btm ${isVisible ? 'visible' : 'hidden'}`} onClick={scrollToTop}>
             <FaAngleUp className="icon-position icon-style" />
         </div>
     );
